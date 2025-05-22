701b69bd4b2624287ef940075304a1992be13bcf
Razzandi <104542077+Razzandi@users.noreply.github.com>
2025-05-21 23:10:46 -0400

    Blah

--- Lists.py
+++
@@ -1,8 +0,0 @@
-#Lists
-import random
-
-List_One = input("Name your first list! :")
-
-print(List_One)
-
-
--- Test.py
+++
@@ -1,9 +0,0 @@
-ip = input("What is the target ip address? ")
-
-def nmap(ip):
-    print(f"Attacking {ip}")
-
-
-
-
-nmap(ip)
